49399aa9f965f885d4fa7c5719fa808a31afbae0
GertMadsen <cph-gm50@cphbusiness.dk>
2018-02-15 13:10:16 +0100

    calculator nu med callbacks

--- src/main/webapp/script1.js
+++ src/main/webapp/script1.js
@@ -4,51 +4,60 @@ document.getElementById("calculate").onclick = calcResult;
 
 document.getElementById("buttons").onclick = getKey;
 
-document.getElementById("home").onclick = function() {  
-    window.location.href='index.html'; };
-
-function getKey(e) {
-  var text = e.target.innerText;
-  calcStr += text;
-  document.getElementById("display").innerHTML = calcStr;
+document.getElementById("home").onclick = function () {
+    window.location.href = 'index.html';
 };
 
+function getKey(e) {
+    var text = e.target.innerText;
+    calcStr += text;
+    document.getElementById("display").innerHTML = calcStr;
+}
+;
+
 function calcResult(e) {
-   e.stopPropagation();
-   var result = 0;
-   if (calcStr.includes("+")) {
-     var numbers = calcStr.split("+");
-     firstNumber = Number(numbers[0]);
-     secondNumber = Number(numbers[1]);
-     result = firstNumber + secondNumber;
-     calcStr = "";
-    } 
-   if (calcStr.includes("-")) {
-     var numbers = calcStr.split("-");
-     firstNumber = Number(numbers[0]);
-     secondNumber = Number(numbers[1]);
-     result = firstNumber - secondNumber;
-     calcStr = "";
+    e.stopPropagation();
+    var result = 0;
+    var cb;
+    if (calcStr.includes("+")) {
+        var numbers = calcStr.split("+");
+        cb = add;
+    }
+    if (calcStr.includes("-")) {
+        var numbers = calcStr.split("-");
+        cb = sub;
     }
     if (calcStr.includes("*")) {
-     var numbers = calcStr.split("*");
-     firstNumber = Number(numbers[0]);
-     secondNumber = Number(numbers[1]);
-     result = firstNumber * secondNumber;
-     calcStr = "";
-    } 
+        var numbers = calcStr.split("*");
+        cb = mul;
+    }
     if (calcStr.includes("/")) {
-     var numbers = calcStr.split("/");
-     firstNumber = Number(numbers[0]);
-     secondNumber = Number(numbers[1]);
-     result = firstNumber / secondNumber;
-     calcStr = "";
-    } 
-     
-   document.getElementById("display").innerHTML = result;
-
-   
+        var numbers = calcStr.split("/");
+        cb = div;
+    }
+    document.getElementById("display").innerHTML = doCalc(numbers, cb);;
 }
 
+function doCalc(numbers, callback) {
+    firstNumber = Number(numbers[0]);
+    secondNumber = Number(numbers[1]);
+    calcStr = "";
+    return callback(firstNumber, secondNumber);
+}
 
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+function mul(n1, n2) {
+    return n1 * n2;
+}
+
+function div(n1, n2) {
+    return n1 / n2;
+}
 
